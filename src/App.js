28be68a1b44c17f97330b7b51207018b0f4b21bc
Carly Lee <carly.l86@gmail.com>
2020-03-10 17:00:29 +0100

    feat1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I want to lie down.</p>
+        <p>feat 1</p>
       </header>
     </div>
   );
